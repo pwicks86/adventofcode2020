204537cadc095074866449c8e9d11a01ae90ffc8
Paul Wicks <pwicks86@gmail.com>
2020-12-15 22:27:20 -0800

    Day 15 complete

---
+++ 15/2.py
@@ -0,0 +1,22 @@
+from collections import defaultdict, deque
+with open("input.txt") as f:
+    data = [int(x) for x in f.read().split(",")]
+
+nums = {n:1 for n in data}
+spoken_turn = {n:deque([i + 1], 2) for i,n in enumerate(data)}
+nums = defaultdict(int, nums)
+turn_num = len(nums) + 1
+spoken = data[-1]
+while True:
+    if nums[spoken] == 1:
+        spoken = 0
+    else:
+        spoken = abs(spoken_turn[spoken][0] - spoken_turn[spoken][1])
+    if spoken not in spoken_turn:
+        spoken_turn[spoken] = deque([],2)
+    spoken_turn[spoken].append(turn_num)
+    nums[spoken] += 1
+    if (turn_num == 30000000):
+        print(spoken)
+        break
+    turn_num += 1
